6eb21edd99d688fa3b930bdfbea2c50da65a299a
Muhammed Imran <imranmuhammed2000@gmail.com>
2020-06-03 21:49:05 -0500

    make server respond to all game types

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ console.log('started server');
 const io = require("socket.io");
 const server = io.listen(5000);
 
-const gameChoices = ['spyfall', 'tictactoe', 'war'];
+const gameChoices = ['/spyfall', '/tictactoe', '/war'];
 const gameMD = new Map();
 
 gameChoices.forEach(game => {
   gameMD.set(game, {
-    nsp: server.of(`/${game}`),
+    nsp: server.of(game),
     rooms: [],
   })
 })
@@ -16,20 +16,23 @@ gameChoices.forEach(game => {
 const players = new Map();
 
 // event fired every time a new client connects:
-server.on("connection", (socket) => {
-  console.info(`Client connected [id=${socket.id}]`);
-
-  // When use first connects, they send over some data about what game they are playing
-  // They also provide their name.
-  socket.on('initialConnection', function (data) {
-    console.log(data);
-    players.set(socket.id, data);
-  })
+gameChoices.forEach(game => {
+  server.of(game).on("connection", (socket) => {
+    console.info(`Client connected [id=${socket.id}] and is playing ${game}`);
   
-
-  // when socket disconnects, remove it from the list:
-  socket.on("disconnect", () => {
-    players.delete(socket.id);
-    console.info(`Client gone [id=${socket.id}]`);
+    // When use first connects, they send over some data about what game they are playing
+    // They also provide their name.
+    socket.on('initialConnection', function (data) {
+      console.log(data);
+      players.set(socket.id, data);
+    })
+    
+  
+    // when socket disconnects, remove it from the list:
+    socket.on("disconnect", () => {
+      players.delete(socket.id);
+      console.info(`Client gone [id=${socket.id}]`);
+    });
   });
-});
+})
+
