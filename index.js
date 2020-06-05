d83d5b93c7d28b171541fb830aa36d3b9a8b5728
Muhammed Imran <imranmuhammed2000@gmail.com>
2020-06-04 23:23:19 -0500

    handle room creation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log('started server');
 
 const io = require("socket.io");
 const server = io.listen(5000);
+//http:localhost:5000/:game/:roomId
 
 const gameChoices = ['/spyfall', '/tictactoe', '/war'];
 const gameMD = new Map();
@@ -14,24 +15,48 @@ gameChoices.forEach(game => {
 })
 
 const players = new Map();
+const socketIDtoPlayer = new Map();
 
 // event fired every time a new client connects:
 gameChoices.forEach(game => {
   server.of(game).on("connection", (socket) => {
     console.info(`Client connected [id=${socket.id}] and is playing ${game}`);
   
-    // When use first connects, they send over some data about what game they are playing
+    // When user first connects, they send over some data about what game they are playing
     // They also provide their name.
     socket.on('initialConnection', function (data) {
       console.log(data);
-      players.set(socket.id, data);
+      data.disconnectTime = -1;
+      socketIDtoPlayer.set(socket.id, data.uuid);
+      players.set(data.uuid, data);
     })
+
+    socket.on('createRoom', function(uuid) {
+      console.log(uuid);
+      socket.join(uuid);
+      console.log('created a room');
+      console.log(socket.rooms);
+      socket.emit('createdRoom', uuid);
+    });
     
     // when socket disconnects, remove it from the list:
-    socket.on("disconnect", () => {
-      players.delete(socket.id);
+    // also keep a time stamp since last login for player
+    socket.on("forceDisconnect", function(){
+      const player = socketIDtoPlayer.get(socket.id);
+      const playerData = players.get(player);
+      playerData.disconnectTime = Date.now();
+      socket.disconnect();
       console.info(`Client gone [id=${socket.id}]`);
     });
+
+    socket.on("disconnect", () => {
+      const player = socketIDtoPlayer.get(socket.id);
+      const playerData = players.get(player);
+      playerData.disconnectTime = Date.now();
+      console.info(`Client gone [id=${socket.id}]`);
+    });
+
+    socket.on
   });
 })
 
