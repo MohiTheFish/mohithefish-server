985caef953af7b7b38663e4ded4d69f7a426fb7f
Muhammed Imran <imranmuhammed2000@gmail.com>
2020-06-06 22:31:12 -0500

    remove console logs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 
-console.log('started server');
-
 import io, { Socket } from "socket.io";
 import { v4 as uuid } from 'uuid';
 import Player from './player';
@@ -69,7 +67,6 @@ gameChoices.forEach(game => {
     // When user first connects, they send over some data about what game they are playing
     // They also provide their name.
     socket.on('initialConnection', function (data: PlayerData) {
-      // console.log(data);
       // For disconnect, we want to be able to determine which player was the one that connected.
       socketToUserId.set(socket.id, data.userId);
       const player = userIdToPlayer.get(data.userId);
@@ -86,7 +83,6 @@ gameChoices.forEach(game => {
       const player = userIdToPlayer.get(userId)!;
       // If player is already in a room
       if (player.roomname) {
-        console.log(`${player.username} was in ${player.roomname}`);
         const roomname = player.roomname;
         // Remove them from that room
         socket.leave(roomname);
@@ -111,16 +107,12 @@ gameChoices.forEach(game => {
           newRoom = new Room(newRoomName, player);
         }
       }
-      console.log('created room');
-      console.log(newRoom);
       
       // Add it to our dictionary
       rooms.set(newRoomName, newRoom);
 
       // Add it to list of rooms available for this game.
       nameSpaceToRooms.get(game)!.push(newRoom);
-
-      console.log(newRoom);
 
       // Return room info.
       socket.emit('createdRoom', newRoom.getRoomInfo());
@@ -154,7 +146,6 @@ gameChoices.forEach(game => {
       // @TODO - Add checks for max occupancy.
       // Gonna do some additional cleaning here. 
       const candidateRooms = nameSpaceToRooms.get(game)!;
-      console.log(candidateRooms);
       const availableRooms: Array<ConciseRoomInfo> = candidateRooms.map(room => room.getConciseRoomInfo());
       socket.emit('availableRooms', availableRooms);
     });
@@ -169,7 +160,6 @@ gameChoices.forEach(game => {
         const player = userIdToPlayer.get(userId)!;
         room.addPlayer(player);
 
-        console.log(`${userId} connecting to ${targetRoom}`);
         // Inform everyone currently in the room that someone else has joined.
         const roomInfo = room.getRoomInfo();
         server.of(game).to(targetRoom).emit('othersJoined', roomInfo);
@@ -187,7 +177,6 @@ gameChoices.forEach(game => {
       const roomname = player.roomname;
       const room = (rooms.get(roomname)!);
       const gameState = (<SpyfallRoom> room).begin();
-      console.log(gameState);
       server.of(game).to(roomname).emit('gameStarted', gameState);
     });
 
@@ -210,8 +199,6 @@ gameChoices.forEach(game => {
       const player = userIdToPlayer.get(userId)!;
       const roomname = player.roomname;
       if (roomname) {
-        console.log(roomname);
-        
         socket.leave(roomname);
         const room = rooms.get(roomname)!;
         let index = -1;
@@ -223,9 +210,7 @@ gameChoices.forEach(game => {
         }
         server.of(game).to(roomname).emit('playerLeft', index);
       }
-      console.log(`Player who left: ${player.username}`);
       player.disconnectPlayer();
-      console.info(`Client gone [id=${socket.id}]`);
     });
 
   });
@@ -233,7 +218,6 @@ gameChoices.forEach(game => {
 
 server.of('/spyfall').on("connection", (socket: Socket) => {
   socket.on('hello world', function(data: any) {
-    console.log(data);
     socket.emit('print', 'welcome');
   })
 })
